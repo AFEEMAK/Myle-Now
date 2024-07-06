28a329d224c7b175d9fa3bb2ab3fa258bcc9ab51
Rifa <prasla38@gmail.com>
2024-07-06 16:17:24 -0400

    Footer updated

--- myle-now/src/App.js
+++ myle-now/src/App.js
@@ -7,14 +7,12 @@ import Nopage from './pages/NoPage';
 import Register from './pages/Register';
 import Services from './pages/Services';
 import ServiceDetails from './pages/ServiceDetails';
-<<<<<<< HEAD
-import CheckoutPage from './pages/Checkout'; 
-
-function App() {
-  const { user } = useAuthContext();
-=======
 import EmailVerify from './pages/EmailVerify';
 import AddService from './pages/AddService';
+import Cart from './pages/Cart';
+import Checkout from './pages/Checkout';
+import OrderDetailsSP from './pages/OrderDetailsSP';
+import OrderDetailsCS from './pages/OrderDetailsCS';
 
 function App() {
   const { user } = useAuthContext();
@@ -24,7 +22,6 @@ function App() {
      role  = user.role
     console.log(role)
   }
->>>>>>> d644ec552a16337e821a5d5cf20625ffc7e790ad
 
   return (
     <>
@@ -34,17 +31,15 @@ function App() {
         <Route path='/login' element={!user ? <Login /> : <Navigate to='/' />} />
         <Route path='*' element={<Nopage />} />
         <Route path='/register' element={!user ? <Register /> : <Navigate to='/' />} />
-<<<<<<< HEAD
-        <Route path='/services/:id' element={<Services />} />
-        <Route path='/serviceDetails/:id' element={<ServiceDetails />} />
-        <Route path='/checkout' element={<CheckoutPage />} /> {}
-=======
-    
         <Route path='/admin/add/service' element={role === 'admin' ? <AddService /> : <Navigate to='/'/> } />
         <Route path='/services/:id' element={<Services />} />
         <Route path='/users/:id/verify/:token' element={<EmailVerify />} />
         <Route exact path='/serviceDetails/:id' element={<ServiceDetails />} />
->>>>>>> d644ec552a16337e821a5d5cf20625ffc7e790ad
+        <Route exact path='/cart' element={<Cart />} />
+        <Route exact path='/ordersCS' element={<OrderDetailsCS />} />
+        <Route exact path='/ordersSP' element={<OrderDetailsSP />} />
+        <Route exact path='/checkout' element={<Checkout />} />
+        
       </Routes>
     </>
   );
