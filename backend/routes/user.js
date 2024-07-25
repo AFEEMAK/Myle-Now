a068c457f01b999a5c3db8b11034abbf160eb4ec
Parth Konkatti <147103528+PARTHKONKATTI@users.noreply.github.com>
2024-07-25 11:20:43 +0530

    Update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -8,12 +8,14 @@ const {
   loginUser,
   signupUser,
   getMe,
+  logout,
 } = require("../controllers/userController");
 const requireAuth = require("../middleware/requireAuth");
 
 router.post("/login", loginUser);
 router.post("/signup", signupUser);
-router.get("/", requireAuth, getMe);
+router.get("/me", requireAuth, getMe);
+router.get("/logout", logout);
 
 router.get("/users/:id/verify/:token", async (req, res) => {
   try {
